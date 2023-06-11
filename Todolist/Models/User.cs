7cbebd96ed996040741cd3406877be70ba744aa3
Ondrej <valentao1291@gmail.com>
2023-06-11 15:57:08 +0200

    #2 Create models

---
+++ Todolist/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Todolist.Models
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+    }
+}
