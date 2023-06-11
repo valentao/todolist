992907f98780189c5e757041833257b9e7b7d36d
Ondrej <valentao1291@gmail.com>
2023-06-11 16:37:18 +0200

    #3 add context
    update gitignore

--- Todolist/Program.cs
+++ Todolist/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Todolist.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<TodolistContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Todolist")));
 
 var app = builder.Build();
 
